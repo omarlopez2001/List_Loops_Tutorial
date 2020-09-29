047811dfb9e610c305c26716036af8547f91e50e
omarlopez2001 <omarlopez9445@gmail.com>
2020-09-29 00:42:39 -0400

    Completed remaining steps

--- list_loops.py
+++ list_loops.py
@@ -1,1 +1,15 @@
 songs = ["ROCKSTAR", "Do It", "For The Night"]
+print(songs[0])
+print(songs[2])
+print(songs[1:3])
+songs[2] = "We Paid"
+print(songs)
+songs.extend(["Come & Go", "Martin & Gina", "bloody valentine"])
+del songs[1]
+
+animals = ["wolf", "lion", "shark"]
+animals.append("bear")
+print(animals[2])
+del animals[0]
+for animal in animals:
+    print(animal)
